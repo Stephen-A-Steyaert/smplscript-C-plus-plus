f7b79387ed6b408c1b67a49a359202af70667608
Stephen Steyaert <savagery2005@gmail.com>
2025-03-14 15:47:21 -0400

    Added blank main file; constant file
    Added the base main file and the first constants.

---
+++ smplscript-c++/constants.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <string>
+
+/*
+Constants for Lexer, Parser. Will eventually contain keywords
+for defining functions, while and for loops, etc.
+*/
+
+namespace constants {
+
+	const std::string TT_INT = "TT_INT";
+	const std::string TT_PLUS = "TT_PLUS";
+	const std::string TT_MINUS = "TT_MINUS";
+	const std::string TT_MUL = "TT_MUL";
+	const std::string TT_DIV = "TT_DIV";
+	const std::string TT_LPAREN = "TT_LPAREN";
+	const std::string TT_RPAREN = "TT_RPAREN";
+
+}
+
+#endif
